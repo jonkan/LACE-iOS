3a5b83d09e0b290769bd9d3b31c2ec31057fa9be
Jonas Bromö <jonas.bromo@gmail.com>
2017-12-26 23:02:09 +0100

    Added the built LACE.framework and Eigen.framework

---
+++ build/framework/Eigen.framework/Headers/Eigen.h
@@ -0,0 +1,19 @@
+//
+//  Eigen.h
+//  Eigen
+//
+//  Created by Jonas Bromö on 2017-12-25.
+//  Copyright © 2017 Jonas Bromö. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Eigen.
+FOUNDATION_EXPORT double EigenVersionNumber;
+
+//! Project version string for Eigen.
+FOUNDATION_EXPORT const unsigned char EigenVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Eigen/PublicHeader.h>
+
+
---
+++ build/framework/LACE.framework/Headers/LACE.h
@@ -0,0 +1,27 @@
+//
+//  LACE.h
+//  LACE-iOS
+//
+//  Created by Jonas Bromö on 2017-12-25.
+//  Copyright © 2017 Jonas Bromö. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LACE.
+FOUNDATION_EXPORT double LACEVersionNumber;
+
+//! Project version string for LACE.
+FOUNDATION_EXPORT const unsigned char LACEVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LACE/PublicHeader.h>
+
+#import <LACE/LpcParameters.hh>
+#import <LACE/LpcProcess.hh>
+#import <LACE/LpcEvent.hh>
+#import <LACE/LpcHitCollection.hh>
+#import <LACE/LpcHit.hh>
+#import <LACE/LpcCurve.hh>
+#import <LACE/LpcBranchCollection.hh>
+#import <LACE/LpcBranch.hh>
+#import <LACE/LpcPoint.hh>
